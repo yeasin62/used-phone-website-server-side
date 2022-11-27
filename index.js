85aa0ff9c4d76d59586eceb1f8d956bca7095d3d
yeasin62 <gazi.yeasin@yahoo.com>
2022-11-28 04:12:27 +0600

    jwt token api created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -23,11 +24,26 @@ async function run (){
         const phoneCollection = client.db("usedPhoneStore").collection("phoneDetails");
         const usersCollection = client.db("usedPhoneStore").collection("users");
         const bookingCollection = client.db("usedPhoneStore").collection("booking");
+        const categoryCollection = client.db("usedPhoneStore").collection("categories");
 
         app.get('/phones', async(req,res)=>{
             const query = {};
             const options = await phoneCollection.find(query).toArray();
             res.send(options);
+        })
+
+        app.get('/categories', async(req,res)=>{
+            const query = {};
+            const options = await categoryCollection.find(query).toArray();
+            res.send(options);
+        })
+
+        app.get('/category/:id', async(req,res)=> {
+            const id = req.params.id;
+            const query = {};
+            const allphones = await phoneCollection.find(query).toArray();
+            const findCat = allphones.filter(n => n.category === id);
+            res.send(findCat);
         })
 
         app.get('/phone/:id', async(req,res)=>{
@@ -37,10 +53,38 @@ async function run (){
             res.send(singlePhone);
         })
 
+        app.get('/orders', async(req,res)=>{
+            const email = req.query.email;
+            const query = {email: email};
+            const orders = await phoneCollection.find(query).toArray();
+            res.send(orders);
+        })
+
+        app.get('/sellers', async(req,res)=>{
+            const seller = req.query.sellerAccount;
+            console.log(seller);
+            const query = {sellerAccount: seller};
+            const sellers = await usersCollection.find(query).toArray();
+            res.send(sellers);
+        })
+
         app.post('/add', async(req,res)=>{
             const phone = req.body;
             const result = await phoneCollection.insertOne(phone);
             res.send(result);
+        })
+
+        app.get('/jwt', async(req,res)=>{
+            const email = req.query.email;
+            const query = {userEmail: email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email},process.env.ACCESS_TOKEN,{expiresIn: '1h'});
+                res.send({accessToken: token});
+            }
+            else {
+                res.status(403).send({accessToken: ''});
+            }
         })
 
         app.post('/users', async(req,res)=>{
